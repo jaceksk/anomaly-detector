e7a4c00d86eb666938457663a3bede4d81c3ae15
Jacek <skierski59@gmail.com>
2024-02-29 12:46:45 +0100

    clean code

--- anomaly-detector-stream/src/test/java/com/art/anomaly/detector/core/stream/config/RoomMeasurementTestTopologyDriver.java
+++ anomaly-detector-stream/src/test/java/com/art/anomaly/detector/core/stream/config/RoomMeasurementTestTopologyDriver.java
@@ -20,29 +20,29 @@ public class RoomMeasurementTestTopologyDriver {
     static final String INPUT_TOPIC = "room-measurement";
     static final String OUTPUT_TOPIC = "room-anomaly";
 
-    private Serde<MeasurementAnomaly> countEventSerde = new JsonSerde<>(MeasurementAnomaly.class);
-    private Serde<TemperatureMeasurementMessage> updateEventSerde = new JsonSerde<>(TemperatureMeasurementMessage.class);
+    private Serde<MeasurementAnomaly> outputEventSerde = new JsonSerde<>(MeasurementAnomaly.class);
+    private Serde<TemperatureMeasurementMessage> inputMessageSerde = new JsonSerde<>(TemperatureMeasurementMessage.class);
     private Serde<String> keySerde = Serdes.String();
     private final TopologyTestDriver testDriver;
     private TestInputTopic<String, TemperatureMeasurementMessage> inputTopic;
     private TestOutputTopic<String, MeasurementAnomaly> outputTopic;
 
     public RoomMeasurementTestTopologyDriver(AnomalyDetector anomalyDetector) {
-        configureDeserializer(countEventSerde.deserializer(), String.class, MeasurementAnomaly.class, false);
+        configureDeserializer(outputEventSerde.deserializer(), String.class, MeasurementAnomaly.class, false);
         configureDeserializer(keySerde.deserializer(), String.class, null, true);
 
         final StreamsBuilder builder = new StreamsBuilder();
 
-        KStream<String, TemperatureMeasurementMessage> input = builder.stream(INPUT_TOPIC, Consumed.with(keySerde, updateEventSerde));
-        AnomalyDetectorProcess inventoryAggregator = new AnomalyDetectorProcess(new AnomalyDetectorService(anomalyDetector));
+        KStream<String, TemperatureMeasurementMessage> input = builder.stream(INPUT_TOPIC, Consumed.with(keySerde, inputMessageSerde));
+        AnomalyDetectorProcess anomalyDetectorProcess = new AnomalyDetectorProcess(new AnomalyDetectorService(anomalyDetector));
 
-        KStream<String, MeasurementAnomaly> output = inventoryAggregator.process().apply(input);
+        KStream<String, MeasurementAnomaly> output = anomalyDetectorProcess.process().apply(input);
         output.to(OUTPUT_TOPIC);
 
         Topology topology = builder.build();
         testDriver = new TopologyTestDriver(topology, getStreamsConfiguration());
-        inputTopic = testDriver.createInputTopic(INPUT_TOPIC, keySerde.serializer(), updateEventSerde.serializer());
-        outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC, keySerde.deserializer(), countEventSerde.deserializer());
+        inputTopic = testDriver.createInputTopic(INPUT_TOPIC, keySerde.serializer(), inputMessageSerde.serializer());
+        outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC, keySerde.deserializer(), outputEventSerde.deserializer());
     }
 
     public void close() {
@@ -66,7 +66,7 @@ public class RoomMeasurementTestTopologyDriver {
     }
 
     public List<MeasurementAnomaly> loadOutputMessages(int expectedCount) {
-        List<MeasurementAnomaly> inventoryCountEvents = new LinkedList<>();
+        List<MeasurementAnomaly> outputsEvents = new LinkedList<>();
         int receivedCount = 0;
         while (receivedCount < expectedCount) {
             MeasurementAnomaly record = outputTopic.readValue();
@@ -74,9 +74,9 @@ public class RoomMeasurementTestTopologyDriver {
                 break;
             }
             receivedCount++;
-            inventoryCountEvents.add(record);
+            outputsEvents.add(record);
         }
-        return inventoryCountEvents;
+        return outputsEvents;
     }
 
     static Properties getStreamsConfiguration() {
